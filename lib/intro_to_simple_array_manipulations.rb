7c64b70eaa582d4eb619d3811c393bf843776f06
William Stevens <wstevens.hornet@gmail.com>
2019-09-06 06:24:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,30 @@
 def using_push(array, string)
   array.push(string)
 end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array, string)
+  array.pop(string)
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array, string)
+  array.shift(string)
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_concat(array, arrays)
+  array.concat(arrays)
+end
+
+def using_uniq(array)
+  array.uniq
